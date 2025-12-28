c8985079291c6267e4d1cc45a0764814a02275c3
efondo-aaronkyle <akyleefondo@gmail.com>
2025-12-29 00:42:36 +0800

    add idp navbar

---
+++ src/features/idp/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 bg-red-800 text-white h-16 flex items-center px-4 z-50 shadow">
+      <h1 className="font-bold">PUP Taguig IdP</h1>
+    </nav>
+  );
+}
