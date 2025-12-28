2d35c58ac857f405c7af2daeb1fbdbb5a0a13927
efondo-aaronkyle <akyleefondo@gmail.com>
2025-12-29 00:43:03 +0800

    add one portal page

---
+++ src/features/one-portal/pages/OnePortalHome.jsx
@@ -0,0 +1,9 @@
+export default function OnePortalHome() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">
+        ONE PORTAL HOME PAGE
+      </h1>
+    </div>
+  );
+}
