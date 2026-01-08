fc97a67ceedcab5b78b20b39167dd76f890c454c
efondo-aaronkyle <akyleefondo@gmail.com>
2026-01-09 00:05:55 +0800

    fix modal ui

--- src/features/idp/components/app-client/AppClientModal.jsx
+++ src/features/idp/components/app-client/AppClientModal.jsx
@@ -47,59 +47,87 @@ export default function AppClientModal({ open, mode, client, onClose, onSubmit }
 
   return (
     <dialog className="modal modal-open">
-      <form className="modal-box bg-white w-full max-w-xl sm:max-w-4xl rounded-xl max-h-[80vh] flex flex-col overflow-y-auto" onSubmit={handleSubmit}>
-        {/* Close button */}
-        <button type="button" className="btn btn-sm btn-circle text-gray-700 hover:bg-transparent border-none btn-ghost absolute right-2 top-2" onClick={onClose}>
-          ✕
-        </button>
-
-        {/* Modal title */}
-        <h3 className="text-sm sm:text-2xl font-bold text-[#991b1b] mb-5">
-          {mode === "create" ? "Create App Client" : mode === "edit" ? "Edit App Client" : "View App Client"}
-        </h3>
-
-        {/* Form fields */}
-        <div className="space-y-4 flex-1">
-          {(mode === "view" || mode === "edit") && (
-            <input type="text" value={client?.clientId} placeholder="Client ID" readOnly className="w-full px-3 py-2 rounded-md border bg-gray-100 text-gray-700 border-gray-300"/>
-          )}
-          
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="client_name" placeholder="Client name (e.g., My Web App)" required className={`w-full px-3 py-2 rounded-lg border border-gray-300 ${
-              mode === "view" ? "bg-gray-100 text-gray-700" : "bg-transparent text-gray-700 focus:ring-2 focus:ring-[#991b1b]"
-            }`} disabled={mode === "view"}/>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <textarea value={callbacks} onChange={(e) => setCallbacks(e.target.value)} name="callback_urls" rows="3" placeholder="Callback URLs (comma-separated)" className={`w-full px-3 py-2 rounded-md border border-gray-300 resize-none max-h-40 overflow-y-auto foucs:outline-none ${
-              mode === "view" ? "bg-gray-100 text-gray-700" : "bg-transparent text-gray-700 focus:ring-2 focus:ring-[#991b1b]"}`} disabled={mode === "view"}/>
-            <textarea value={logouts} onChange={(e) => setLogouts(e.target.value)} name="logout_urls" rows="3" placeholder="Sign out URLs (comma-separated)" className={`w-full px-3 py-2 rounded-md border border-gray-300 resize-none max-h-40 overflow-y-auto foucs:outline-none ${
-              mode === "view" ? "bg-gray-100 text-gray-700" : "bg-transparent text-gray-700 focus:ring-2 focus:ring-[#991b1b]"}`}  disabled={mode === "view"}/>
+      <div className="modal-box max-w-2xl max-h-[85vh] p-0 overflow-hidden flex flex-col">
+        <div className="bg-linear-to-r from-[#991b1b] to-red-600 p-6 text-white shrink-0">
+          <div className="flex items-center justify-between">
+            <div>
+              <h3 className="text-2xl font-bold">
+                {mode === "create" ? "Create App Client" : mode === "edit" ? "Edit App Client" : "View App Client"}
+              </h3>
+            </div>
+            <button className="btn btn-sm btn-circle btn-ghost text-white hover:bg-white/20" onClick={onClose}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
           </div>
-
-          <div className="mb-5">
-            <span className="block text-sm font-medium text-gray-700">Allowed scopes</span>
-            <div className="mt-2 grid grid-cols-2 gap-3 sm:grid-cols-4">
-              {["openid", "profile", "email", "phone"].map((scope) => (
-                <label key={scope} className="flex items-center gap-2 text-gray-700">
-                  <input type="checkbox" name="scopes" value={scope} className="checkbox border-gray-300 bg-transparent checked:bg-[#991b1b] checked:border-red-900 checked:text-white mr-1" checked={selectedScopes.includes(scope)} onChange={() => toggleScope(scope)} disabled={mode === "view"} />
-                  <span className="text-[#991b1b] text-[.7rem] sm:text-sm">{scope}</span>
+        </div>
+        <form id="app-client-form" className="flex-1 overflow-y-auto p-6 space-y-4 bg-white" onSubmit={handleSubmit}>
+          <div className="space-y-4 flex-1">
+            {(mode === "view" || mode === "edit") && (
+              <div className="space-y-0.5">
+                <label className="block text-sm font-semibold text-gray-700">
+                  Client Id
                 </label>
-              ))}
+                <input type="text" value={client?.clientId} placeholder="Client ID" readOnly className="w-full px-3 py-2 rounded-md border bg-gray-100 text-gray-700 border-gray-300"/>
+              </div>
+            )}
+            
+            <div className="space-y-0.5">
+              <label className="block text-sm font-semibold text-gray-700">
+                  Client Name
+                </label>
+              <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="client_name" placeholder="Client name (e.g., My Web App)" required className={`w-full px-3 py-2 rounded-lg border border-gray-300 ${
+                mode === "view" ? "bg-gray-100 text-gray-700" : "bg-transparent text-gray-700 focus:ring-2 focus:ring-[#991b1b]"
+              }`} disabled={mode === "view"}/>
+            </div>
+            
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="space-y-0.5">
+                <label className="block text-sm font-semibold text-gray-700">
+                  Callback URLs
+                </label>
+                <textarea value={callbacks} onChange={(e) => setCallbacks(e.target.value)} name="callback_urls" rows="3" placeholder="Callback URLs (comma-separated)" className={`w-full px-3 py-2 rounded-md border border-gray-300 resize-none max-h-40 overflow-y-auto foucs:outline-none ${
+                mode === "view" ? "bg-gray-100 text-gray-700" : "bg-transparent text-gray-700 focus:ring-2 focus:ring-[#991b1b]"}`} disabled={mode === "view"}/>
+              </div>
+              <div className="space-y-0.5">
+                <label className="block text-sm font-semibold text-gray-700">
+                  Signout URLs
+                </label>
+                <textarea value={logouts} onChange={(e) => setLogouts(e.target.value)} name="logout_urls" rows="3" placeholder="Sign out URLs (comma-separated)" className={`w-full px-3 py-2 rounded-md border border-gray-300 resize-none max-h-40 overflow-y-auto foucs:outline-none ${
+                mode === "view" ? "bg-gray-100 text-gray-700" : "bg-transparent text-gray-700 focus:ring-2 focus:ring-[#991b1b]"}`}  disabled={mode === "view"}/>
+              </div>
+            </div>
+
+            <div className="mb-5">
+              <span className="block text-sm font-medium text-gray-700">Allowed scopes</span>
+              <div className="mt-2 grid grid-cols-2 gap-3 sm:grid-cols-4">
+                {["openid", "profile", "email", "phone"].map((scope) => (
+                  <label key={scope} className="flex items-center gap-2 text-gray-700">
+                    <input type="checkbox" name="scopes" value={scope} className="checkbox border-gray-300 bg-transparent checked:bg-[#991b1b] checked:border-red-900 checked:text-white mr-1" checked={selectedScopes.includes(scope)} onChange={() => toggleScope(scope)} disabled={mode === "view"} />
+                    <span className="text-[#991b1b] text-[.7rem] sm:text-sm">{scope}</span>
+                  </label>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+        </form>
 
         {/* Action buttons */}
-        <div className="flex items-center justify-end gap-3 mt-4">
-          <button type="button" className="btn btn-outline text-[#991b1b] border-[#991b1b] hover:bg-[#ffd700] hover:border-[#ffd700] hover:text-[#991b1b]" onClick={onClose}>
-            Cancel
-          </button>
-          {mode !== "view" && (
-            <button type="submit" className="btn bg-[#991b1b] text-white border-[#991b1b] hover:bg-[#ffd700] hover:border-[#ffd700] hover:text-[#991b1b]">
-              {mode === "create" ? "Create" : "Save"}
+        <div className="p-6 bg-gray-50 border-t border-gray-200 shrink-0">
+          <div className="flex justify-end gap-3">
+            <button type="button" className="btn h-12 rounded-lg btn-outline text-[#991b1b] border-[#991b1b] hover:bg-[#ffd700] hover:border-[#ffd700] hover:text-[#991b1b]" onClick={onClose}>
+              Cancel
             </button>
-          )}
+            {mode !== "view" && (
+              <button form="app-client-form" type="submit" className="btn h-12 rounded-lg bg-[#991b1b] text-white border-[#991b1b] hover:bg-[#ffd700] hover:border-[#ffd700] hover:text-[#991b1b]">
+                {mode === "create" ? "Create" : "Save"}
+              </button>
+            )}
+          </div>
         </div>
-      </form>
+      </div>
     </dialog>
   );
 }
