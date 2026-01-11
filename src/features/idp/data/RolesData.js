2250be189ed2c86bbd68fc4593d0037e44d72b97
efondo-aaronkyle <akyleefondo@gmail.com>
2026-01-11 15:37:00 +0800

    fix roles id

--- src/features/idp/data/RolesData.js
+++ src/features/idp/data/RolesData.js
@@ -1,6 +1,6 @@
 export const initialRoles = [
   {
-    id: 1,
+    id: 3,
     role_name: "dental:admin",
     description:
       "Admin responsible for managing inventory, staff access, system settings, and operational records",
@@ -14,7 +14,7 @@ export const initialRoles = [
     created_at: "2024-06-22",
   },
   {
-    id: 3,
+    id: 1,
     role_name: "registrar:admin",
     description:
       "Administrative officer responsible for verifying student records, managing enrollment data, and issuing certifications",
