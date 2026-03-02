783cef1bd6252d9cb4c26a0a4fb0b27b1b7303a2
efondo-aaronkyle <akyleefondo@gmail.com>
2026-03-03 00:32:54 +0800

    change cursor to default

--- src/features/idp/components/app-client/AppClientModal.jsx
+++ src/features/idp/components/app-client/AppClientModal.jsx
@@ -156,7 +156,7 @@ export default function AppClientModal({ open, mode, client, onClose, onSubmit }
                 } ${mode === "view" ? "border-gray-200 cursor-default" : "hover:bg-gray-100 cursor-pointer"}`}
               >
                 {!imagePreview ? (
-                  <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-full cursor-pointer">
+                  <label htmlFor="dropzone-file" className={`flex flex-col items-center justify-center w-full h-full ${ mode === "view" ? "cursor-default" : "cursor-pointer"}`}>
                     <div className="flex flex-col items-center justify-center pt-5 pb-6 text-center">
                       <svg className={`w-8 h-8 mb-2 transition-colors ${isDragging ? "text-[#991b1b]" : "text-gray-400"}`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
